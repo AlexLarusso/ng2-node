d4b9cdea4fc8ee02bee06a323370f8f39dc40199
Oleksandr Vasyliev <oleksandr.vasyliev@eleks.com>
2016-11-07 21:56:28 +0200

    es6 refactoring

--- api/api.js
+++ api/api.js
@@ -1,49 +1,56 @@
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+'use strict';
 
-// Connection URL
-var url = 'mongodb://localhost:27017/ng2-node';
-// Use connect method to connect to the server
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+const MongoClient = require('mongodb').MongoClient,
+  url = 'mongodb://localhost:27017/ng2-node',
+  assert = require('assert');
 
-// Connection URL
-var url = 'mongodb://localhost:27017/myproject';
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
+class DB {
+  constructor() {
+  }
 
-  insertDocuments(db, function() {
-    findDocuments(db, function() {
-      db.close();
+  init() {
+    return new Promise(resolve => {
+      MongoClient.connect(url, (err, db) => {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+        this.db = db;
+        resolve();
+      });
+    })
+  }
+
+  close() {
+    this.db.close();
+  }
+
+  insertDocuments(data, callback) {
+    let collection = this.db.collection('documents');
+
+    collection.insertMany(data, (err, result) => {
+      assert.equal(err, null);
+      assert.equal(3, result.result.n);
+      assert.equal(3, result.ops.length);
+      console.log("Inserted 3 documents into the collection");
     });
-  });
-});
+  }
 
-var insertDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('documents');
-  // Insert some documents
-  collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
+  findDocuments(callback) {
+    let collection = this.db.collection('documents');
+    // Find some documents
+    collection.find({})
+      .toArray((err, docs) => {
+        assert.equal(err, null);
+        console.log("Found the following records");
+        console.log(docs);
+      });
+  }
+
+  removeDocuments(callback) {
+    let collection = this.db.collection('documents');
+
+    collection.drop();
+  }
+
 }
 
-var findDocuments = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('documents');
-  // Find some documents
-  collection.find({}).toArray(function(err, docs) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(docs)
-    callback(docs);
-  });
-}
+module.exports = new DB();
---
+++ api/main.js
@@ -0,0 +1,14 @@
+'use strict';
+(() => {
+const db = require('./api'),
+  mocks = [
+    {a : 1}, {a : 2}, {a : 3}
+  ];
+
+  db.init().then(() => {
+    db.removeDocuments();
+    db.insertDocuments(mocks);
+    db.findDocuments();
+  });
+
+})();
